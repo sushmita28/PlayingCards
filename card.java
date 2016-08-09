6ce377d849c19419963cb380d82ef3e989d9810b
skarthikeya <karthikeya.surabhi@gmail.com>
2016-08-09 14:31:23 +0530

    Adding card class

---
+++ card.java
@@ -0,0 +1,41 @@
+package playCards;
+
+public class card {
+	String cardValue;
+	String cardSuit;
+	
+	int type;    
+	int value;
+	
+	private static String[] VALUES = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13" };
+	
+	private static String[] SUITS = { "HEARTS", "DIAMONDS", "SPADES", "CLUBS" };
+    
+	public card(int cardSuit, int cardValue) {
+		this.cardValue = card.VALUES[cardValue+1];
+		this.cardSuit = card.SUITS[cardSuit+1];
+	}
+
+	public String getCardValue() {
+
+		return this.cardValue;
+
+	}
+
+	public String getCardSuit() {
+		return this.cardSuit;
+	}
+
+	public boolean isSuitMatch(card c) {
+		return c.getCardSuit() == this.cardSuit;
+	}
+	
+	public boolean isValueMatch(card c){
+		return c.getCardValue()==this.cardValue;
+	}
+	
+	
+	
+	
+
+}
